5c291356ec6723b6405aa94143f5014f5593ed6d
Max <max@MacBook-Pro---max.local>
2026-03-21 12:31:34 +0300

    init logger

---
+++ core/log_config.py
@@ -0,0 +1,36 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path(__file__).with_name(".env.core"))
+
+LOG_LEVEL = os.getenv('LOG_LEVEL')
+
+def setup_logging():
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    logger = logging.getLogger()
+    logger.setLevel({
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL
+    }[LOG_LEVEL])
+
+    handler = TimedRotatingFileHandler(
+        filename=os.path.join(log_dir, "app.log"),
+        when="midnight",
+        interval=1,
+        backupCount=7,
+        encoding='utf-8'
+    )
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    ))
+
+    if not logger.handlers:  # чтобы не дублировать случайно логгер
+        logger.addHandler(handler)
